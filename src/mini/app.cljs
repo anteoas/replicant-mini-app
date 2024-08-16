52445963793febc76162a1e73e1bbde023c481df
Peter Strömberg <pez@pezius.com>
2024-08-16 17:24:47 +0200

    Add enrichment to the actions

--- src/mini/app.cljs
+++ src/mini/app.cljs
@@ -1,38 +1,74 @@
 (ns mini.app
-  (:require [replicant.dom :as r]))
+  (:require [clojure.walk :as walk]
+            [replicant.dom :as r]))
 
-(defonce ^:private !state (atom {:ui/splash? true}))
+(defonce ^:private !state (atom {}))
 
-(defn- sub-view [{:something/keys [something]}]
-  [:div {:replicant/on-mount [[:something/init-something]]}
-   (when something
-     [:p "Something: " something])])
+(defn- edit-view []
+  [:form {:on {:submit [[:dom/prevent-default]
+                        [:db/assoc :something/saved [:db/get :something/draft]]]}}
+   [:input {:on {:input [[:db/assoc :something/draft :event/target.value]]}}]
+   [:button {:type :submit} "Save draft"]])
 
-(defn- add-button []
-  [:button {:on {:click [[:db/assoc :something/something :something]]}} "Add something"])
+(defn- display-view [{:something/keys [draft saved]}]
+  [:div
+   [:p "Draft: " draft]
+   [:p "Saved: " saved]])
 
 (defn- main-view [state]
-  [:div
-   (sub-view state)
-   (add-button)])
+  [:div {:replicant/on-mount [[:something/init-something :dom/node]]
+         :style {:position "relative"}}
+   [:h1 "Hello, world!"]
+   (edit-view)
+   (display-view state)])
 
 (defn- render! [state]
   (r/render
    (js/document.getElementById "app")
    (main-view state)))
 
-(defn- event-handler [_replicant-data actions]
+(defn- enrich-action-from-event [{:replicant/keys [js-event node]} actions]
+  (letfn [(process [x]
+            (cond
+              (keyword? x)
+              (case x
+                :event/target.value (-> js-event .-target .-value)
+                :dom/node node
+                x)
+
+              (coll? x) (into (empty x) (map process x))
+
+              :else x))]
+    (process actions)))
+
+(defn- enrich-action-from-state [state action]
+  (walk/postwalk
+   (fn [x]
+     (cond
+       (and (vector? x)
+            (= :db/get (first x))) (get state (second x))
+       :else x))
+   action))
+
+(defn- event-handler [{:replicant/keys [js-event] :as replicant-data} actions]
   (doseq [action actions]
     (prn "Triggered action" action)
-    (case (first action)
-      :something/init-something (reset! !state {:something/initialized true})
-      :db/assoc (apply swap! !state assoc (rest action))
-      (prn "Unknown action" action)))
-  (render! @!state))
+    (let [enriched-action (->> action
+                               (enrich-action-from-event replicant-data)
+                               (enrich-action-from-state @!state))]
+      (prn "Enriched action" enriched-action)
+      (case (first enriched-action)
+        :dom/prevent-default (.preventDefault js-event)
+        :something/init-something (swap! !state merge {:something/dom-node (second enriched-action)})
+        :db/assoc (apply swap! !state assoc (rest enriched-action))
+        (prn "Unknown action" enriched-action)))))
 
 (defn ^{:dev/after-load true :export true} start! []
   (render! @!state))
 
 (defn ^:export init! []
   (r/set-dispatch! event-handler)
+  (add-watch !state :render (fn [_k _r o n]
+                              (when (not= o n)
+                                (render! @!state))))
   (start!))
