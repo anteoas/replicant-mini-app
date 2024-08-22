969f32cd1c9961b5f4a194f720d990a706ef6b52
Peter Strömberg <pez@pezius.com>
2024-08-22 13:44:16 +0200

    Add clear draft button

--- src/mini/app.cljs
+++ src/mini/app.cljs
@@ -14,32 +14,31 @@
    [:button {:on {:click [[:ui/dismiss-banner]]}} "Dismiss"]])
 
 (defn- edit-view []
-  [:form {:on {:submit [[:dom/prevent-default]
-                        [:db/assoc :something/saved [:db/get :something/draft]]]}}
-   [:input#draft {:on {:input [[:db/assoc :something/draft :event/target.value]]}}]
-   [:button {:type :submit} "Save draft"]])
+  [:div
+   [:form {:on {:submit [[:dom/prevent-default]
+                         [:db/assoc :something/saved [:db/get :something/draft]]]}}
+    [:input#draft {:replicant/on-mount [[:db/assoc :something/draft-input-element :dom/node]]
+                   :on {:input [[:db/assoc :something/draft :event/target.value]]}}]
+    [:button {:type :submit} "Save draft"]]
+   [:button {:on {:click [[:db/assoc :something/draft ""]
+                          [:dom/set-input-text [:db/get :something/draft-input-element] ""]
+                          [:dom/focus-element [:db/get :something/draft-input-element]]]}}
+    "Clear draft"]])
 
-(defn- display-view [{:something/keys [draft saved dom-node]}]
+(defn- display-view [{:something/keys [draft saved]}]
   [:div
    [:h2 "On display"]
    [:ul
     [:li {:replicant/key "draft"} "Draft: " draft]
-    [:li {:replicant/key "saved"} "Saved: " saved]
-    (when dom-node
-      [:li {:replicant/key "dom-node"} "ID of something dom-node: " [:code (.-id dom-node)]])]])
-
-(defn- something-view []
-  [:div#something-something {:replicant/on-mount [[:something/init-something :dom/node]]}
-   [:p "Something, something"]])
+    [:li {:replicant/key "saved"} "Saved: " saved]]])
 
 (defn- main-view [state]
   [:div {:style {:position "relative"}}
    (when (:ui/banner-text state)
      (banner-view state))
-   [:h1 "A tiny Replicant example"]
+   [:h1 "A tiny (and silly) Replicant example"]
    (edit-view)
-   (display-view state)
-   (something-view)])
+   (display-view state)])
 
 (defn- enrich-action-from-event [{:replicant/keys [js-event node]} actions]
   (letfn [(process [x]
@@ -84,9 +83,9 @@
                                     (swap! !state merge {:something/dom-node (first args)}))
         :db/assoc (apply swap! !state assoc (rest enriched-action))
         :ui/dismiss-banner (swap! !state dissoc :ui/banner-text)
-        :dom/fx-set-text (set! (.-value (first args)) (second args))
-        :dom/fx-focus-element (.focus (first args)
-        (prn "Unknown action" action)))))
+        :dom/set-input-text (set! (.-value (first args)) (second args))
+        :dom/focus-element (.focus (first args))
+        (prn "Unknown action" action))))
   (render! @!state))
 
 (defn ^{:dev/after-load true :export true} start! []
